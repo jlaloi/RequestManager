6341eeab513fa53ce0f9c0b2b4e04110ab9686ee
Julien Laloi <julien.laloi@gmail.com>
2015-12-06 19:14:59 +0100

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,24 @@ var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var del = require('del');
 var path = require('path');
-var copy = require('copy');
 
 var $ = gulpLoadPlugins();
 
 var dist = "target/classes/static/";
 
-gulp.task('lint', function () {
-    gulp.src('/src/main/resources/static/js/*.js')
-        .pipe($.eslint())
-});
-
 gulp.task('build', ["clean"], function () {
-    gulp.src('src/main/resources/static/*.html')
-        .pipe($.useref())
-        .pipe($.if('*.css', $.minifyCss()))
-        .pipe($.if('*.js', $.ngAnnotate()))
-        .pipe($.if('*.js', $.uglify()))
-        .pipe(gulp.dest(dist));
-    copy(path.join('src/main/resources/static/templates/'), path.join(dist));
+    return gulp.src('src/main/resources/static/*.html')
+            .pipe($.useref())
+            .pipe($.if('*.css', $.minifyCss()))
+            .pipe($.if('*.js', $.ngAnnotate()))
+            .pipe($.if('*.js', $.uglify()))
+            .pipe(gulp.dest(dist))
+        & gulp.src('src/main/resources/static/templates/*.html')
+            .pipe(gulp.dest(dist + 'templates/'));
 });
 
-gulp.task('clean' , function () {
-    del(path.join(dist));
-
+gulp.task('clean', function () {
+    return del(path.join(dist));
 });
-
 
 
